0eef67cbdf9ff4890e3f23b8c11f9eb738bec3a6
Ali <alimbzw@gmail.com>
2022-08-01 00:53:48 -0700

    first copy

---
+++ src/main/java/com/alisexample/awss3bootstrap/AwsS3BootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.alisexample.awss3bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3BootstrapApplication.class, args);
+	}
+
+}
